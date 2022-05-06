7e96213c727500058b4783316eb2863b9c9e7cd1
TKHTS <sakuranohanabira1269@gmail.com>
2022-05-06 10:39:32 -0700

    To add config path

--- coach/index.php
+++ coach/index.php
@@ -1,28 +1,29 @@
 <?php
-    include('./header.php');
+include("../config/config.php");
+include('./header.php');
 ?>
 <main>
     <?php
-        if(isset($_GET['p'])){
-            switch($_GET['p']){
-                case "home":
-                    include ('./pages/home.php');
+    if (isset($_GET['p'])) {
+        switch ($_GET['p']) {
+            case "home":
+                include('./pages/home.php');
                 break;
-                case "add_level_to_member":
-                    include('./pages/add_level_to_member.php');
-                    break;
-                case "edit_level_of_member":
-                    include('./pages/edit_level_of_member.php');
-                    break;
-                case "freeboard":
-                    include('./pages/freeboard.php');
-                    break;
-            }
-        }else{
-            include ('./pages/home.php');
+            case "add_level_to_member":
+                include('./pages/add_level_to_member.php');
+                break;
+            case "edit_level_of_member":
+                include('./pages/edit_level_of_member.php');
+                break;
+            case "freeboard":
+                include('./pages/freeboard.php');
+                break;
         }
+    } else {
+        include('./pages/home.php');
+    }
     ?>
 </main>
 <?php
-    include('./footer.php');
+include('./footer.php');
 ?>
--- member/index.php
+++ member/index.php
@@ -1,4 +1,5 @@
 <?php
+    include("../config/config.php");
     include('./header.php');
 ?>
 <main>
