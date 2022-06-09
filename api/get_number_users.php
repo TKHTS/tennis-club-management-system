2183379d45798722490100690c77bbcb5e4d9b66
CristobalRocha <labastida.cristobal@gmail.com>
2022-06-08 18:20:06 -0700

    FrontPage2 is done

--- api/get_number_users.php
+++ api/get_number_users.php
@@ -1,6 +1,21 @@
-<!-- Chris writes here...
+<?php
+include('./config.php');
+$sqlCon = new mysqli($servername, $username, $password, $dbname);
 
-Number of coach,
-Number of Member,
+if ($sqlCon->connect_error) {
+    exit('DB Error');
+}
+$selectCommandCoach = "SELECT COUNT( user_name ) FROM users WHERE user_type = 'coach'";
+$selectCommandMember = "SELECT COUNT( user_name ) FROM users WHERE user_type = 'member'";
+$resultCoach = $sqlCon->query($selectCommandCoach);
+$resultMember = $sqlCon->query($selectCommandMember);
 
--->
+$dataCoach = $resultCoach->fetch_column();
+$dataMember = $resultMember->fetch_column();
+
+$output = 
+"<div class='card col-12 p-5'><h2 class='card-title text-primary'>Coach: " . $dataCoach . "</h2></div>" 
+."<div class='card col-12 p-5'><h2 class='card-title text-primary'>Member: ". $dataMember . "</h2></div>";
+
+$sqlCon->close();
+echo $output;
